55f7292a8d65a73dac893796c67f860f11deb324
bigtroublejake <bigtroublejake@gmail.com>
2024-06-11 00:51:40 -0400

    boom

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 # https://github.com/cjhutto/vaderSentiment
 
 # Note to myself while building this:
-# Use the Anaconda interpreter, as that's the only one that seems to allow me to import pandas
+# Use the Anaconda interpreter, as that's the only one that seems to allow me to import pandas. Also run it in the terminal.
 # No clue why it's like this, but it works so don't question it lol
 
 from vaderSentiment import vaderSentiment as vs
@@ -12,8 +12,8 @@ import pandas as pd
 # print(vs.SentimentIntensityAnalyzer().polarity_scores("vader is dumb"))
 
 testSentence = "vader is dumb"
-score = vs.SentimentIntensityAnalyzer().polarity_scores(testSentence)
-print(score["compound"])
+# score = vs.SentimentIntensityAnalyzer().polarity_scores(testSentence)
+# print(score["compound"])
 
 def PosTest(sentence):
     # running sentiment analysis
@@ -29,5 +29,57 @@ def PosTest(sentence):
     elif score["compound"] > -0.05 and score["compound"] < 0.05:
         return 
     
-print(PosTest(testSentence))
+# print(PosTest(testSentence))
+
+testFileDep = pd.read_csv('Test Tweets/dataset_16_emojifinal.csv', usecols=['Text', 'Sentiment'])
+# print(testFileDep.iterrows)
+
+tweetList = []
+correctPos = 0
+incorrectPos = 0
+correctNeg = 0
+incorrectNeg = 0
+
+for index, rows in testFileDep.iterrows():
+    list = rows.Text
+
+    # tweetList.append(str(list))
+    result = PosTest(list)
+
+    # Positive emotions
+    if result == True and (rows.Sentiment == 1 or rows.Sentiment == 3):
+        correctPos += 1
+        print(index, 'C pos')
+    # Negative emotions
+    elif result == False and (rows.Sentiment == 0 or rows.Sentiment == 2):
+        correctNeg += 1
+        print(index, "C neg")
+    # Wrong guess or netural emotion
+    else:
+        if rows.Sentiment == 1 or rows.Sentiment == 3:
+            incorrectPos += 1
+            print(index, 'I pos')
+        elif rows.Sentiment == 0 or rows.Sentiment == 2:
+            incorrectNeg += 1
+            print(index, 'I neg')
+
+    # print(list)
+
+print(correctPos, incorrectPos, correctNeg, incorrectNeg, (correctPos+correctNeg)*100/(correctPos+incorrectPos+correctNeg+incorrectNeg))
+
+
+# correctAssesments = 0
+# for i in range(0,len(tweetList)):
+#     result = PosTest(tweetList[i])
+#     if result == False:
+#         correctAssesments += 1
+#         print("right", i)
+#     else:
+#         print ("wrong", i)
+
+# print(correctAssesments, correctAssesments/len(tweetList)*100)
+
+
+
+
 
